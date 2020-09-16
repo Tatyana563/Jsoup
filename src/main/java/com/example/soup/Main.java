6c4902981d201e42e95ffbc3bb780bdd50acdecf
Tanya <tatyana.petrenko@dstarlab.com>
2020-09-16 17:37:49 +0300

    first

---
+++ src/main/java/com/example/soup/Main.java
@@ -0,0 +1,31 @@
+package com.example.soup;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+@Component
+public class Main {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Scheduled(fixedRate = 30000)
+    public void getElements(){
+      //  System.out.println(restTemplate.getForObject("https://www.w3schools.com/xml/xpath_syntax.asp", String.class));
+        try {
+            Document doc = Jsoup.connect("https://www.w3schools.com/xml/xpath_syntax.asp").get();
+           // System.out.println("TITLE OF THE DOCUMENT: "+doc.attr("data-original-title"));
+          //  System.out.println("TITLE OF THE DOCUMENT: "+doc.select("div p"));
+            System.out.println(doc.select("a[href][title]"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+//<a class="w3-bar-item w3-button" id="topnavbtn_tutorials" href="javascript:void(0);" onclick="w3_open_nav(&quot;tutorials&quot;)" title="Tutorials">MORE <i class="fa fa-caret-down" style="display: inline;"></i><i class="fa fa-caret-up" style="display:none"></i></a>
---
+++ src/main/java/com/example/soup/SoupApplication.java
@@ -0,0 +1,25 @@
+package com.example.soup;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableScheduling
+public class SoupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoupApplication.class, args);
+
+    }
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+
+    }
+}
