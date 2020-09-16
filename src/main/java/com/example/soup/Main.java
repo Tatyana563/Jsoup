8fcc54b280b70ac3a5d42b1921b0fe2fba792b56
Tanya.Petrenko <nikolaev.english@gmail.com>
2020-09-17 00:36:01 +0300

    selectors

--- src/main/java/com/example/soup/Main.java
+++ src/main/java/com/example/soup/Main.java
@@ -20,7 +20,9 @@ public class Main {
             Document doc = Jsoup.connect("https://www.w3schools.com/xml/xpath_syntax.asp").get();
            // System.out.println("TITLE OF THE DOCUMENT: "+doc.attr("data-original-title"));
           //  System.out.println("TITLE OF THE DOCUMENT: "+doc.select("div p"));
-            System.out.println(doc.select("a[href][title]"));
+         //   System.out.println(doc.select(".w3-bar-item.w3-button:not(.topnav-icons)"));
+        //    System.out.println(doc.select(".left:nth-of-type(4) ~ a"));
+            System.out.println(doc.select("#topnavbtn_tutorials"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -28,4 +30,6 @@ public class Main {
     }
 
 }
+//"/python/default.asp"
+//        Learn Python
 //<a class="w3-bar-item w3-button" id="topnavbtn_tutorials" href="javascript:void(0);" onclick="w3_open_nav(&quot;tutorials&quot;)" title="Tutorials">MORE <i class="fa fa-caret-down" style="display: inline;"></i><i class="fa fa-caret-up" style="display:none"></i></a>
